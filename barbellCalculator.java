b0eb920f0dba413e574f967314f01ef61968bcf6
Brendon Williams <bjw0619@gmail.com>
2021-07-01 22:20:27 -0400

    Create barbellCalculator.java

---
+++ barbellCalculator.java
@@ -0,0 +1,22 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class barbellCalculator extends JFrame{
+
+    public barbellCalculator() {
+        JFrame frame = new JFrame("Barbell Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Barbell Calculator");
+        setLayout(new GridBagLayout());
+    }
+    public static void main(String [] args) {
+
+    }
+}
